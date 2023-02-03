c4e4121adb9c88d5e9a282b36998524f7dbbc0a6
Matin KarimPour <matin_karimpour@outlook.com>
2023-02-03 12:56:34 +0330

    create: 01_shoping.py

---
+++ codes/01_shoping.py
@@ -0,0 +1,20 @@
+shoping_list = ["oil", "potato", "tomato", "rice"]
+prices = {
+    "oil": 70,
+    "potato":15,
+    "tomato": 20,
+    "rice": 300
+}
+budget = 400
+basket = []
+
+for item in shoping_list:
+    price = prices[item]
+    if price < budget:
+        basket.append(item)
+        budget = budget - price
+    else:
+        print("budget is not enough for this ", item)
+
+print("your final basket is ", basket)
+print("your final budget is ", budget)    
