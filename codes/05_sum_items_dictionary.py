d320db079e520ea213fc68893c589c742faed0ee
Matin KarimPour <matin_karimpour@pm.me>
2023-02-14 12:50:27 +0330

    add: 05_sum_items_dictionary.py

---
+++ codes/05_sum_items_dictionary.py
@@ -0,0 +1,16 @@
+dictionary1 = {'a':100, 'b':200, 'c':300}
+
+sum1 = 0
+for i in dictionary1:
+    sum1 += dictionary1[i]
+
+print(sum1)
+
+sum2 = sum(dictionary1.values())
+print(sum2)
+
+sum3 = 0
+for i in dictionary1.items():
+    sum3 += i[1]
+
+print(sum3)
