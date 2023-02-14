989d1c8c6e50bbc963a4db31bf82e2030777bad4
Matin KarimPour <matin_karimpour@pm.me>
2023-02-14 20:43:37 +0330

    add: 07_plot_gaussian.py

---
+++ codes/07_plot_gaussian.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def gaussian(mu,sigma,size):
+    x = np.arange(-1,1,1/size)
+    guass = (1/np.sqrt(2*np.pi*np.square(sigma)))*np.exp(-np.square(x-mu)/(2*np.square(sigma)))
+    return guass
+size = 1000
+x = np.arange(-1,1,1/size)
+
+plt.subplot(2,2,1)
+mu,sigma = 0,0.1
+rand_guass = np.random.normal(mu,sigma,size)
+plt.hist(rand_guass,30,density=True)
+plt.plot(x,gaussian(mu,sigma,size))
+plt.title(f"mu: {mu} sigma: {sigma}")
+
+plt.subplot(2,2,2)
+mu,sigma = 0.2,0.1
+rand_guass = np.random.normal(mu,sigma,size)
+plt.hist(rand_guass,30,density=True)
+plt.plot(x,gaussian(mu,sigma,size))
+plt.title(f"mu: {mu} sigma: {sigma}")
+
+mu,sigma = 0,0.5
+plt.subplot(2,2,3)
+rand_guass = np.random.normal(mu,sigma,size)
+plt.hist(rand_guass,30,density=True)
+plt.plot(x,gaussian(mu,sigma,size))
+plt.title(f"mu: {mu} sigma: {sigma}")
+
+mu,sigma = 0.2,0.5
+plt.subplot(2,2,4)
+rand_guass = np.random.normal(mu,sigma,size)
+plt.hist(rand_guass,30,density=True)
+plt.plot(x,gaussian(mu,sigma,size))
+plt.title(f"mu: {mu} sigma: {sigma}")
+
+plt.show()
